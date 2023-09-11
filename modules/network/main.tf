d1697a96cc02eb17a13d54c8285c050582fbb5e5
Yurii <jurakulek@gmail.com>
2023-09-11 23:59:04 +0300

    Refactored networking module

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -55,46 +55,6 @@ resource "aws_subnet" "private-subnet-2" {
   }
 }
 
-# Route tables
-
-resource "aws_route_table" "public-route-table" {
-  vpc_id = aws_vpc.production-vpc.id
-
-  tags = {
-    Name : "${var.env_prefix}-public-route-table"
-  }
-}
-
-resource "aws_route_table" "private-route-table" {
-  vpc_id = aws_vpc.production-vpc.id
-
-  tags = {
-    Name : "${var.env_prefix}-private-route-table"
-  }
-}
-
-# Route table associations
-
-resource "aws_route_table_association" "public-route-1-association" {
-  route_table_id = aws_route_table.public-route-table.id
-  subnet_id      = aws_subnet.public-subnet-1.id
-}
-
-resource "aws_route_table_association" "public-route-2-association" {
-  route_table_id = aws_route_table.public-route-table.id
-  subnet_id      = aws_subnet.public-subnet-2.id
-}
-
-resource "aws_route_table_association" "private-route-1-association" {
-  route_table_id = aws_route_table.private-route-table.id
-  subnet_id      = aws_subnet.private-subnet-1.id
-}
-
-resource "aws_route_table_association" "private-route-2-association" {
-  route_table_id = aws_route_table.private-route-table.id
-  subnet_id      = aws_subnet.private-subnet-2.id
-}
-
 # IGW
 
 resource "aws_internet_gateway" "production-igw" {
@@ -130,16 +90,53 @@ resource "aws_nat_gateway" "nat-gw" {
     Name : "${var.env_prefix}-nat-gw"
   }
 }
-resource "aws_route" "nat-gw-route" {
-  route_table_id         = aws_route_table.private-route-table.id
-  nat_gateway_id         = aws_nat_gateway.nat-gw.id
-  destination_cidr_block = "0.0.0.0/0"
+
+# Route tables
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.production-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.production-igw.id
+  }
+
+  tags = {
+    Name : "${var.env_prefix}-public-route-table"
+  }
 }
 
-# Route public subnet through IGW
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.production-vpc.id
 
-resource "aws_route" "public-igw-route" {
-  route_table_id         = aws_route_table.public-route-table.id
-  gateway_id             = aws_internet_gateway.production-igw.id
-  destination_cidr_block = "0.0.0.0/0"
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gw.id
+  }
+
+  tags = {
+    Name : "${var.env_prefix}-private-route-table"
+  }
+}
+
+# Route table associations
+
+resource "aws_route_table_association" "public-route-1-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-1.id
+}
+
+resource "aws_route_table_association" "public-route-2-association" {
+  route_table_id = aws_route_table.public-route-table.id
+  subnet_id      = aws_subnet.public-subnet-2.id
+}
+
+resource "aws_route_table_association" "private-route-1-association" {
+  route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-1.id
+}
+
+resource "aws_route_table_association" "private-route-2-association" {
+  route_table_id = aws_route_table.private-route-table.id
+  subnet_id      = aws_subnet.private-subnet-2.id
 }
